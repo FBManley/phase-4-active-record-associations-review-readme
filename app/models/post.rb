c4dd5ce9cd63f0953f9d4b97f102652f30a221b5
Jake Manley <jbmanley45@gmail.com>
2022-12-11 12:01:33 -0800

    join table realized

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    # This gives us access to an author method in our Post class.
+    belongs_to :author 
+    has_many :post_tags
+    has_many :tags, through: :post_tags
 end
